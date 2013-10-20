072a835bd874544abc2a99b092999bb084692d73
Robin Cerny <robin@cerny.li>
2013-10-20 23:18:21 +0200

    refurbish the whitepaces and tabs

--- english/smcp.sh
+++ english/smcp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# 	StarMade Control Panel V1.0
-# 	
+#	StarMade Control Panel V1.1
+#
 #	10.10.2013 - Erstellt
-# 	11.10.2013 - PID ueberpruefung eingebaut
+#	11.10.2013 - PID ueberpruefung eingebaut
 #	13.10.2013 - Release
 #	13.10.2013 - screen session überprüfung einkommentiert	
 #
@@ -10,17 +10,18 @@
 #
 #	This program is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation; either version 2 of the License.
+#	the Free Software Foundation; either version 2 of the License or
+#	any later version.
 #
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
 #
-#   	You should have received a copy of the GNU General Public License
+#	You should have received a copy of the GNU General Public License
 #	along with this program; if not, write to the Free Software
-#   	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#   	MA 02110-1301, USA.
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#	MA 02110-1301, USA.
 
 
 # Settings
@@ -86,13 +87,18 @@ serverAlive(){
 
 	#status=$(tcptraceroute -S -w 10 localhost $PORT 2> /dev/null|tail -1|awk '{print $4}')
 	if [ "$status" == "open" ]; then
+
 		echo online
+
 	else
+
 		echo offline
+
 	fi
 }
 
 players(){
+
 	probe="\x00\x00\x00\x09\x2a\xff\xff\x01\x6f\x00\x00\x00\x00"
 	echo -n -e "$probe" | netcat -o $BASEDIR/packet/hex.tmp -v -w 1 localhost $PORT > /dev/null 2>&1
 
@@ -104,18 +110,25 @@ players(){
 }
 
 timestamp(){
+
 	echo "$(date +%d.%m.%y) $(date +%H:%M:%S)"
 }
 
 mobcount(){
+
 	MOB=$(ls -l $BASEDIR/server/server-database/ | grep ENTITY_SHIP_MOB | wc -l)
 	if [ $MOB = 0 ] ; then
+
 		echo "no"
+
 	else
-        echo "$MOB"
+
+		echo "$MOB"
+
 	fi
 }
 
+# restart over parameter
 if [ "$1" = "-r" ] ; then
 	
 	restart
@@ -181,41 +194,41 @@ echo -e "\e[31m		 (__  ) \e[34m/ / / / / /__/ /_/ /      \e[0m"
 echo -e "\e[31m		/____/\e[34m_/ /_/ /_/\___/ .___/       \e[0m"
 echo -e "\e[34m		                   /_/            \e[0m"
 echo -e "
-            StarMade Control Panel V$SMCPVERSION by  \e[31mZ\e[34modiak\e[0m
+            StarMade Control Panel V$SMCPVERSION by \e[31mZ\e[34modiak\e[0m
 
 StarMade Version: 		$SMVERSION
 PID of the StarMade Server:	$SMPID
 
- [1]  start			Start the StarMade Server
- [2]  stop			Stop the StarMade Server
- [3]  restart			Restart the StarMade Server
- [4]  status			Retrieves the status of the server
- [5]  update			Execute an update
- [6]  emerg			Try a 'emergency shutdown'
- [7]  kill			Kills the star made server with violent
- [8]  mobclean			Deletes all 'mob' records from the database
- [9]  reattach			Reattach the screensession '$SCREENSESSION'
- [10] DBSize			Shows the current size of the database
- [11] order			Send a custom command to the StarMade server
- [12] message			Send a custom message to all players
- [13] mkadmin			Make a player admin
- [14] rmadmin			Remove a admin
- [15] mkwhite			Add a Player to the whitelist
- [16] editwhite			Edit the whitelist
- [17] editserver.cfg		Edit the server.cfg
- [18] editcron			Edit the crontab
- [19] editblack			Edit the blacklist
- [20] editmsg			Edit the welcome message
+ [1]	start		Start the StarMade Server
+ [2]	stop		Stop the StarMade Server
+ [3]	restart		Restart the StarMade Server
+ [4]	status		Retrieves the status of the server
+ [5]	update		Execute an update
+ [6]	emerg		Try a 'emergency shutdown'
+ [7]	kill		Kills the StarMade server with violent
+ [8]	mobclean	Deletes all 'mob' records from the database
+ [9]	reattach	Reattach the screensession '$SCREENSESSION'
+ [10]	DBSize		Shows the current size of the database
+ [11]	order		Send a custom command to the StarMade server
+ [12]	message		Send a custom message to all players
+ [13]	mkadmin		Make a player admin
+ [14]	rmadmin		Remove a admin
+ [15]	mkwhite		Add a Player to the whitelist
+ [16]	editwhite	Edit the whitelist
+ [17]	editconfig	Edit the server.cfg
+ [18]	editcron	Edit the crontab
+ [19]	editblack	Edit the blacklist
+ [20]	editmsg		Edit the welcome message
 
- [r]  reload			Press 'r' to update the Control Panel
- [0]  abort			Return to the shell\n"
+ [r]	reload		Press 'r' to update the Control Panel
+ [0]	abort		Return to the shell\n"
 
 read -p "choice: " answer
 
 case $answer in
 
 	1)
-	
+
 	    #if [[ -z $(screen -ls $SCREENSESSION | grep $SCREENSESSION) ]] ; then
 			# session doesn't exist
 			echo "Starting screen session '$SCREENSESSION'..."
@@ -254,7 +267,7 @@ case $answer in
 			sleep 30
 			echo "Shutdown in 30s"
 			sleep 30
-            echo "Shutdown was performed, please check..."
+			echo "Shutdown was performed, please check..."
 			sleep 5
 
 		else
@@ -284,7 +297,7 @@ case $answer in
 	
 		echo "Server is $(serverAlive) with $(players) of max $MAXPLAYERS players."
 		sleep 8
-	    ;;
+		;;
 	    
 	5)
 	
@@ -293,23 +306,23 @@ case $answer in
 		if [ "$updateanswer" = "y" ] ; then
 			
 			read -p "Should the server be shut down? y/n: " stopanswer
-            if [ "$stopanswer" = "y" ] ; then
+			if [ "$stopanswer" = "y" ] ; then
 
 				touch $LOCK
-                screen -S $SCREENSESSION -p 0 -X stuff "/chat SHUTDOWN Server for Backup or Update in 2 minutes!$(printf \\r)"
-                screen -S $SCREENSESSION -p 0 -X stuff "/force_save$(printf \\r)"
-                echo "Initiate shutdown. Time left: 120s"
-                sleep 60
-                screen -S $SCREENSESSION -p 0 -X stuff "/chat SHUTDOWN Server for Backup or Update in 60 seconds!$(printf \\r)"
-                screen -S $SCREENSESSION -p 0 -X stuff "/shutdown 60$(printf \\r)"
-                echo "Shutdown in 60s"
-                sleep 30
-                echo "Shutdown in 30s"
-                sleep 30
-                echo "Shutdown was perfomed. Please check..."
-                echo "Checking server status..."
-                sleep 1
-                
+				screen -S $SCREENSESSION -p 0 -X stuff "/chat SHUTDOWN Server for Backup or Update in 2 minutes!$(printf \\r)"
+				screen -S $SCREENSESSION -p 0 -X stuff "/force_save$(printf \\r)"
+				echo "Initiate shutdown. Time left: 120s"
+				sleep 60
+				screen -S $SCREENSESSION -p 0 -X stuff "/chat SHUTDOWN Server for Backup or Update in 60 seconds!$(printf \\r)"
+				screen -S $SCREENSESSION -p 0 -X stuff "/shutdown 60$(printf \\r)"
+				echo "Shutdown in 60s"
+				sleep 30
+				echo "Shutdown in 30s"
+				sleep 30
+				echo "Shutdown was perfomed. Please check..."
+				echo "Checking server status..."
+				sleep 1
+
 				if [ "$(serverAlive)" = "offline" ] ; then
 				
 					echo "Server has been shut down properly. Proceed with the update..."
@@ -324,10 +337,10 @@ case $answer in
 				
 				fi
 
-            else
+			else
 
-                echo "Abort..."
-                sleep 5
+				echo "Abort..."
+				sleep 5
 			
 			fi
 
@@ -337,7 +350,7 @@ case $answer in
 			sleep 5
 
 		fi
-	    ;;  
+		;;  
 	    
 	6)
 	
@@ -350,20 +363,20 @@ case $answer in
 			echo "Was performed, please check..."
 			sleep 5
 
-	    else
+		else
 		
 			echo "Abort..."
 			sleep 5
 
-	    fi
-	    ;;
-	    
+		fi
+		;;
+
 	7)
 	
 		echo -e "\n\e[31m\e[4mCAUTION!!! When using this function, it IS LOST DATA!\e[0m"
-	    read -p "Really proceed? If yes, write: Yes, I want to continue! : " killanswer
+		read -p "Really proceed? If yes, write: Yes, I want to continue! : " killanswer
 		if [ "$killanswer" = "Yes, I want to continue!" ] ; then
-		
+
 			echo "Grab the sledgehammer..."
 			sleep 2
 			echo "Beat the process down..."
@@ -371,11 +384,11 @@ case $answer in
 			echo "*dong*"
 			sleep 1
 			echo "*dong*"
-	        sleep 2
+			sleep 2
 			echo "*klirr*"
 			sleep 1
 			echo "execute 'kill -9 $SMPID'"
-	    	kill -9 $SMPID
+			kill -9 $SMPID
 			sleep 1
 			echo "Termination of the process is successfully, restarting..."
 			sleep 4
@@ -385,15 +398,15 @@ case $answer in
 			echo "Wrong answer, abort..."
 			sleep 5
 
-	    fi
+		fi
 		;;
 		
 	8)
 	
-	    screen -S $SCREENSESSION -p 0 -X stuff "/force_save$(printf \\r)"
-	    sleep 10
-	    echo "$(mobcount) records of mobs where found in the database."
-	    read -p "Proceed with the deletion? [y/n]:" answer
+		screen -S $SCREENSESSION -p 0 -X stuff "/force_save$(printf \\r)"
+		sleep 10
+		echo "$(mobcount) records of mobs where found in the database."
+		read -p "Proceed with the deletion? [y/n]:" answer
 		if [ "$answer" = "y" ] ; then
 
 			echo "Deleting Mob records..."
@@ -408,55 +421,55 @@ case $answer in
 			echo "Abort..."
 			Sleep 3
 
-	    fi
-	    ;;
-	    
+		fi
+		;;
+
 	9)
-	
+
 		screen -r $SCREENSESSION
-	    sleep 2
-        ;;
-	
+		sleep 2
+		;;
+
 	10)
 
 		echo "calculate..."
-	    sleep 1
-	    du -sch $BASEDIR/server/server-database/ | tail -n 1
-	    sleep 5
+		sleep 1
+		du -sch $BASEDIR/server/server-database/ | tail -n 1
+		sleep 5
 		;;
-		
+
 	11)
-	
-	    echo -e "All StarMade commands are possible. Caution! no feedback .\nExample: /force_save"
-	    read -p "enter the command: " order
-	    screen -S $SCREENSESSION -p 0 -X stuff "$order $(printf \\r)"
-	    sleep 2
+
+		echo -e "All StarMade commands are possible. Caution! no feedback .\nExample: /force_save"
+		read -p "enter the command: " order
+		screen -S $SCREENSESSION -p 0 -X stuff "$order $(printf \\r)"
+		sleep 2
 		;;
 	
 	12)
 	
-	    echo "Attention, only one row is possible!"
-	    read -p "Broadcast Message: " msg
-	    screen -S $SCREENSESSION -p 0 -X stuff "/chat $msg $(printf \\r)"
-	    sleep 2
+		echo "Attention, only one row is possible!"
+		read -p "Broadcast Message: " msg
+		screen -S $SCREENSESSION -p 0 -X stuff "/chat $msg $(printf \\r)"
+		sleep 2
 		;;
-		
+
 	13) 
-	
-	    echo "current admins:"
-	    cat $BASEDIR/server/admins.txt
-	    echo ""
-	    echo "Pay attention to upper/lower case."
-	    read -p "Type username: " mkadmin
-	    screen -S $SCREENSESSION -p 0 -X stuff "/add_admin $mkadmin $(printf \\r)"
+
+		echo "current admins:"
+		cat $BASEDIR/server/admins.txt
+		echo ""
+		echo "Pay attention to upper/lower case."
+		read -p "Type username: " mkadmin
+		screen -S $SCREENSESSION -p 0 -X stuff "/add_admin $mkadmin $(printf \\r)"
 		sleep 5
 		echo "current admins:"
 		cat $BASEDIR/server/admins.txt
 		sleep 5
 		;;
-       
+
 	14)
-    
+
 		echo "current admins:"
 		cat $BASEDIR/server/admins.txt
 		echo ""
@@ -479,10 +492,10 @@ case $answer in
 	16)
 
 		echo "open file..."
-	    sleep 2
+		sleep 2
 		$EDITOR $BASEDIR/server/whitelist.txt
 		sleep 1
-	    ;;
+		;;
 
 	17)
 
@@ -500,13 +513,13 @@ case $answer in
 		;;
 		
 	18)
-	
+
 	    crontab -e
 	    sleep 3
 	    ;;
-	
+
 	19)
-    
+
 		echo "open file..."
 		sleep 2
 		$EDITOR $BASEDIR/server/blacklist.txt
@@ -521,22 +534,23 @@ case $answer in
 		;;
 
 	0)
-	
+
 		echo "Exit the StarMade Control Panel..."
 		sleep 1
-	    break
+		break
 		;;
-    
+
 	r)
-    
-        echo "reload"
-        sleep 1 
+
+		echo "reload"
+		sleep 1 
 		;;
-	
+
 	*)
-	
+
 	    echo "unknown parameter, return back to the menu"
 	    sleep 1
+
 esac
 
 done
