b3f628aaf9cf3d0f2f465e2c8cbda95c4300a2d5
Robin Cerny <robin@cerny.li>
2013-10-22 22:39:19 +0200

    remove some trailing whitespaces and tabs

--- start.sh
+++ start.sh
@@ -45,46 +45,46 @@ if [ -f "$PIDFILE" ] ; then
 
 	RUNNINGPID=$(cat "$PIDFILE")
 	PROGRAMPID=$(ps ax | grep "$SCRIPTNAME" | grep -v grep | awk '{print $1;}')
-    
-    for PIDEL in $PROGRAMPID
-    do
-	
+
+	for PIDEL in $PROGRAMPID
+	do
+
 		if [ "$PIDEL" == "$RUNNINGPID" ] ; then
-		
-            ENDEXECUTION=1
-            break
-    
+
+			ENDEXECUTION=1
+			break
+
 		fi
-		
+
     done
 
 fi
 
 if [ "$ENDEXECUTION" == "1" ] ; then
 
-    echo "It is already a server running on port $PORT. Finish..."
-    exit 1
-    
+	echo "It is already a server running on port $PORT. Finish..."
+	exit 1
+
 fi
 # write PID in pidfile
 echo $PID > $PIDFILE
 
 while true; do
 
-    echo "start $(currentSecs)" > $STATE
-    echo "$(timestamp) Running command: '$cmd'" | tee -a $log
-    $cmd
-    if [ -f $LOCK ] ; then
-		
+	echo "start $(currentSecs)" > $STATE
+	echo "$(timestamp) Running command: '$cmd'" | tee -a $log
+	$cmd
+	if [ -f $LOCK ] ; then
+
 		shutdowncmd
-    
-    else
-        
-	    	echo "$(timestamp) Server 'StarMade' crashed with exit code $?.  Restarting..." | tee -a $log
+
+	else
+
+		echo "$(timestamp) Server 'StarMade' crashed with exit code $?.  Restarting..." | tee -a $log
 		echo "" > $ADMINS
-    
-    fi
-    
-		sleep 1
+
+	fi
+
+	sleep 1
 
 done
